6d1fb4080402a5f0482d915bc6d35c84be4bd1f8
Alva Chien <alvachien@163.com>
2025-02-27 15:42:44 +0800

    Add environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+
+    LoginRequired: true,
+    CurrentVersion: '1.6.457',
+    ReleasedDate: '2023.12.31',
+    DefaultLanguage: 'en',
+
+    DebugLogging: true,
+    LoggingLevel: 4,
+    IDServerUrl: 'https://localhost:44353',
+    AppLogoutCallbackUrl: 'https://localhost:29521',
+    AppHost: 'https://localhost:29521',
+    ApiUrl: 'https://localhost:44360',
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+
+    LoginRequired: true,
+    CurrentVersion: '1.6.457',
+    ReleasedDate: '2023.12.31',
+    DefaultLanguage: 'zh',
+  
+    DebugLogging: false,
+    LoggingLevel: 2,
+    IDServerUrl: 'https://www.alvachien.com/idserver',
+    AppLogoutCallbackUrl: 'https://www.alvachien.com/hih',
+    AppHost: 'https://www.alvachien.com/hih',
+    ApiUrl: 'https://www.alvachien.com/hihapi',    
+};
